58108c2e6f9980d3863b9947399409fbab4924ee
railskipl <rails@kunalinfotech.net>
2012-07-02 16:25:42 +0530

    spree gem installed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spreedemo::Application.config.secret_token = '66e50eeb2112938d354fc061677ee14467ed4716e5c054b928300cad9b0b79765b04e21e8a3b1ff7dd515d657eda439b37a41380e4fa8dd230f5bb0a5f799775'

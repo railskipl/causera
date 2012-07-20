846367873c96c7cccc59e7266d6ffcd60678f816
KIPL <kipl@kipl-Inspiron-620s.(none)>
2012-07-20 17:16:35 +0530

    title changed

---
+++ app/overrides/title.rb
@@ -0,0 +1,4 @@
+Deface::Override.new(:virtual_path  => "spree/shared/_head",
+                     :replace_contents => "title",
+                     :text          => "Causera",
+                     :name          => "Title")
